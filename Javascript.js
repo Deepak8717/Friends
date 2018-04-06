137365d7daddb0c7ed83d6f2e247cf97364723ad
Deepak Pundir <sendit2deepak@gmail.com>
2018-04-06 07:29:49 +0530

    first commit

---
+++ Javascript.js
@@ -0,0 +1,32 @@
+var quoteOptions = [{
+    quote: "“Let Go and Let God..”",
+    name: "Marilyn Monroe"
+  }, {
+    quote: "“Don't cry because it's over, smile because it happened.”",
+    name: "Dr. Seuss"
+  }, {
+    quote: "“Be yourself; everyone else is already taken.”",
+    name: "Oscar Wilde"
+  }, {
+    quote: "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”",
+    name: "Albert Einstein"
+  }, {
+    quote: "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”",
+    name: " Bernard M. Baruch"
+  }, {
+    quote: "“You know you're in love when you can't fall asleep because reality is finally better than your dreams.”",
+    name: "Dr. Seuss"
+  }, {
+    quote: "“A room without books is like a body without a soul.”",
+    name: "Marcus Tullius Cicero"
+  }, {
+    quote: "“So many books, so little time.”",
+    name: "Frank Zappa"
+  }, {
+    quote: "“You only live once, but if you do it right, once is enough.”",
+    name: "Mae West"
+  }, {
+    quote: "“Be the change that you wish to see in the world.”",
+    name: "Mahatma Gandhi"
+  }, ];
+  
